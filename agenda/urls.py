2d03b8ed7f43ddc18dcbf323c717013a11e50fe6
Frederico Gustavo Magalhães <fredlgprime@gmail.com>
2020-06-23 20:19:34 -0300

    Criação da página Agendamento
    Criação do diretório Templates e criação da tela de eventos.

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('eventos/<titulo_evento>', views.consulta_eventos),
+    path('agenda/', views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/')),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
+
 
 # Create your views here.
+
+# def index(request):
+#     return redirect('/agenda')
+
+
+# def consulta_eventos(request, evento):
+#     if evento:
+#         Evento.objects.get(titulo=evento)
+#         return Evento.titulo
+#     else:
+#         return HttpResponse('A pesquisa não retornou nenhum registro.')
+
+def lista_eventos(request):
+    # usuario = request.user
+    # evento = Evento.objects.filter(usuario=usuario)
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
