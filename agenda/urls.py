379ca47f740ac701f15f4613ed197d476e64a3a3
Frederico Gustavo Magalhães <fredlgprime@gmail.com>
2020-06-30 15:55:47 -0300

    Template de inserção
    Incluída a tela de inserção de novos eventos.

--- agenda/urls.py
+++ agenda/urls.py
@@ -22,9 +22,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('eventos/<titulo_evento>', views.consulta_eventos),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
     path('logout/', views.logout_user),
 
 ]
