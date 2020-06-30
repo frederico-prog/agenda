2105ea13a37a822719935701265761708009abd3
Frederico Gustavo Magalhães <fredlgprime@gmail.com>
2020-06-30 16:56:29 -0300

    Campo LOCAL
    Adicionado o campo Local na tela de Eventos. Adicionanda nova coluna na tabela de Evento. Inserida na ação de inserção os dados do campo Local.

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
+    local = models.CharField(max_length=100, null=True)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
--- core/views.py
+++ core/views.py
@@ -57,10 +57,12 @@ def evento(request):
 def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
+        local = request.POST.get('local')
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
         Evento.objects.create(titulo=titulo,
+                              local=local,
                               data_evento=data_evento,
                               descricao=descricao,
                               usuario=usuario)
